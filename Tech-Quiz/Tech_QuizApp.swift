ad24e698db49e3897d821e0306cc31b04e539a19
Brenno Natal <natalbrenno@gmail.com>
2021-10-29 18:34:21 -0300

    Initial Commit

---
+++ Tech-Quiz/Tech_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_QuizApp.swift
+//  Tech-Quiz
+//
+//  Created by Brenno Natal on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Tech_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
