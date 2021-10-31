e1b8b6b99fc5b1e6a4164cb2ba845c519fac94f6
grinch1 <hugotntop@gmail.com>
2021-10-31 16:09:17 -0300

    Dark mode

--- Tech-Quiz/ContentView.swift
+++ Tech-Quiz/ContentView.swift
@@ -72,5 +72,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
+
